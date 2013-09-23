8f8225902ed5f4ec6cc1fcef3f920f251efe66c8
Nigel Cleland <nigel.cleland@gmail.com>
2013-09-24 09:48:06 +1200

    Trying out a double templating strategy

--- ninja_tables/ninja_tables.py
+++ ninja_tables/ninja_tables.py
@@ -9,7 +9,27 @@ import sys
 
 # Get the environment
 
-def render_table(file_name, template_name="template_name.tex"):
+def render_template(template_name, size):
+    """ Render a Template which will then be used to render the table
+
+    """
+
+    header = " & ".join(["%{{ header.%s }}%" % x for x in range(size)])
+    data = " & ".join(["%{{ item.%s }}%" % x for x in range(size)])
+    tab_columns = "l"*(size + 1)
+    begin_block = "%{ for item in data }%"
+    end_block = "%{ endfor }%"
+
+    renderer = define_renderer()
+    template = renderer.get_template(template_name)
+    intermediate_file = 'intermediate.tex'
+
+    with open(intermediate_file, 'w') as f:
+        f.write(template.render((header, data, tab_columns)))
+
+    return intermediate_file
+
+def define_renderer():
     renderer = jinja2.Environment(
         block_start_string='%{',
         block_end_string='}%'
@@ -18,13 +38,19 @@ def render_table(file_name, template_name="template_name.tex"):
         loader=jinja2.FileSystemLoad(os.path.abspath('.'))
         )
 
-    template=renderer.get_template(template_name)
+    return renderer
 
-    # Get the data here
-    headers = None
-    data = None
+def render_table(file_name, template_name="template_name.tex"):
 
-    entries = len(headers) * 'l'
+    renderer = define_renderer()
+    intermediate = render_template(template_name, len(headers))
+    template = renderer.get_template(intermediate)
+    new_filename = file_name.replace('.csv', '.tex')
+
+    with open(new_filename, 'w') as f:
+        f.write(template.render((headers, data)))
+
+
 
 
 if __name__ == '__main__':
